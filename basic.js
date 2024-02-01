3698129f007df59fed61e21b8cd49b91d25e9226
harshil1008 <158443740+harshil1008@users.noreply.github.com>
2024-02-01 03:35:12 -0800

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,10 @@
+console.log("helloo world\n HARSHIL BOSS\n RAM RAM");
+
+
+let username;
+document.getElementById("mybutton").onclick=function(){
+
+    username=document.getElementById("mytext").ariaValueMax;
+    console.log(username);
+
+}
